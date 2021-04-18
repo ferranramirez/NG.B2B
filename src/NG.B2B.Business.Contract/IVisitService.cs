f0b68590ff636f0618e6ba0b4d676ffd492d02c1
Ferran Ramirez <ferran@notguiri.com>
2021-04-18 03:55:07 +0200

    Add GET Visits endpoint NG-332

---
+++ src/NG.B2B.Business.Contract/IVisitService.cs
@@ -0,0 +1,12 @@
+﻿using NG.DBManager.Infrastructure.Contracts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NG.B2B.Business.Contract
+{
+    public interface IVisitService
+    {
+        Task<IEnumerable<VisitInfo>> GetByCommerce(Guid commerceId, Guid authUserId);
+    }
+}
